5adb9f40c6c4e20cbe62c4cc3adb9eb702388f09
gebreabfanuel <138772466+gebreabfanuel@users.noreply.github.com>
2023-07-07 12:02:26 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function showmenu() {
+        var menuO = document.getElementById("menuO");
+        menuO.style.right = "0";
+    }
+
+    function hidemenu() {
+        var menuO = document.getElementById("menuO");
+        menuO.style.right = "-200px";
+    }
